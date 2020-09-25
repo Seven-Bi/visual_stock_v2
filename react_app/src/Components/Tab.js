56909e61d18b697024d65c2408101f47cfc527c4
BB <seven.albany.bi@gmail.com>
2020-09-25 20:35:28 +0800

    finish rough version

--- react_app/src/Components/Tab.js
+++ react_app/src/Components/Tab.js
@@ -9,24 +9,37 @@ import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
+import Chip from '@material-ui/core/Chip';
+import Typography from '@material-ui/core/Typography';
+import Card from '@material-ui/core/Card';
 
 
 
-
-
+let shade_tab_data = {}
+let msg = ''
 function change(tab_data, row) {
-	let l_price = 'none (click start)'
 	if(tab_data.length > 0) {
 		let res = JSON.parse(tab_data)
-		console.log(res.data)
-		for (var i = res.data.length - 1; i >= 0; i--) {
+		for (var i = 0; i < res.data.length; i++) {
 			if(res.data[i].s === row.s) {
-				l_price = res.data[i].c
-				return l_price
+				if(row.s in shade_tab_data) {
+					msg = `${row.s}  <Latest Price>  updated from ${shade_tab_data[row.s]} to ${res.data[i].c}!`
+				}
+				else {
+					msg = `${row.s}  <Latest Price>  updated to ${res.data[i].c}!`
+				}
+				
+				shade_tab_data[row.s] = res.data[i].c
+				return res.data[i].c
+			}
+			else{
+				if(row.s in shade_tab_data) {
+					return shade_tab_data[row.s]
+				}
+				return row.c
 			}
 		}
 	}
-	return l_price
 }
 
 function TabPanel(props) {
@@ -44,10 +57,9 @@ function TabPanel(props) {
 					<TableHead>
 						<TableRow>
 							<TableCell>Pair</TableCell>
-							<TableCell align="right">Parent Market</TableCell>
-							<TableCell align="right">Category of PM</TableCell>
-							<TableCell align="right">Open Price</TableCell>
-							<TableCell align="right">Latest Price </TableCell>
+							<TableCell align="left">Parent Market</TableCell>
+							<TableCell align="left">Open Price</TableCell>
+							<TableCell align="left">Latest Price </TableCell>
 						</TableRow>
 					</TableHead>
 					<TableBody>
@@ -56,10 +68,9 @@ function TabPanel(props) {
 							<TableCell component="th" scope="row">
 								{row.s}
 							</TableCell>
-							<TableCell align="right">{row.pm}</TableCell>
-							<TableCell align="right">{row.pn}</TableCell>
-							<TableCell align="right">{row.o}</TableCell>
-							<TableCell align="right">{change(props.tab_data, row)}</TableCell>
+							<TableCell align="left">{row.pm}</TableCell>
+							<TableCell align="left">{row.o}</TableCell>
+							<TableCell align="left">{change(props.tab_data, row)}</TableCell>
 						</TableRow>
 					))}
 					</TableBody>
@@ -106,7 +117,7 @@ export default function ScrollableTabsButtonAuto(props) {
 
 	const tab_data = Object.keys(props.tab_base).map((key, index_num) => {
 		return(
-			<TabPanel key={key} tab_data={props.tab_data} data_list={props.tab_base[key].slice(0,4)} value={value} index={index_num}>
+			<TabPanel key={key} tab_data={props.tab_data} data_list={props.tab_base[key].slice(0,10)} value={value} index={index_num}>
 			</TabPanel>
 		)
 	})
@@ -127,6 +138,32 @@ export default function ScrollableTabsButtonAuto(props) {
 				</Tabs>
 				{tab_data}
 			</AppBar>
+			<div style={{ marginTop: '20px'}}>
+				<Chip 
+					style={{ minWidth: '100px'}}
+					label={ msg }
+					color="secondary"	
+				/>
+			</div>
+			<div style={{ marginTop: '20px'}}>
+				<Card>
+					<Typography style={{ textAlign: 'center'}} variant="h4" gutterBottom>
+						Simulate Crypto Exchange UI Demo
+					</Typography>
+					<Typography style={{ textAlign: 'center'}} variant="h5" gutterBottom>
+						A brief to functionalities:
+					</Typography>
+					<Typography style={{ textAlign: 'center'}} variant="button" gutterBottom>:
+						<li>load open price and latest price</li>
+						<li>see real-time data (latest price)</li>
+						<li>start/pause real time function</li>
+					</Typography>
+					<Typography style={{ textAlign: 'center'}} variant="body1" gutterBottom>
+						Update info will be updated in red chip as well, will take some time to
+						waite sample data get update.
+					</Typography>
+				</Card>
+			</div>			
 
 		</div>
 	);
